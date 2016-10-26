4aee7b05e1f0202f820ca9b573ac52e0d5fc3fca
Breena Li <bli022@hammer.cs.ucr.edu>
2016-10-25 23:19:58 -0700

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+
+
 #include "rshell.h"
+
 
 using namespace std;
 
 int main() 
 {
-	string user_input;	
+	string user_input;
+	string quit = "exit";
+
+
+	cout << '$';
+	getline(cin,user_input);
+
+	char *cmd_arr = new char[user_input.size()];
+	char *token = strtok(cmd_arr, "&;|");
 	
-	cout << "Enter a command: " << endl;
-	cin >> user_input;
+	while (token != NULL) 
+	{
+		if (user_input.compare(quit) != 0)
+		{
+			token = strtok(NULL, "&;|");
+		}
+		else
+		{
+			exit(0);
+		}	
+	}
+
+
 
 
 	return 0;
