f53b0a0cd74ac09944512d294a8f695f9d5683fc
Breena Li <bli022@hammer.cs.ucr.edu>
2016-10-25 23:52:39 -0700

    fixed placement of if statement in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <stdio.h>
-#include <string.h>
-#include <cstdlib>
-
 
 #include "rshell.h"
 
@@ -16,26 +12,23 @@ int main()
 	string user_input;
 	string quit = "exit";
 
-
-	cout << '$';
-	getline(cin,user_input);
-
-	char *cmd_arr = new char[user_input.size()];
-	char *token = strtok(cmd_arr, "&;|");
+	cout << "$ ";
+	getline(cin, user_input);
 	
-	while (token != NULL) 
+	while(user_input.compare(quit) != 0)
 	{
-		if (user_input.compare(quit) != 0)
+		char *cmd_arr = new char[user_input.size()];
+		char *token = strtok(cmd_arr, "&;|");
+
+		while (token != NULL) 
 		{
+		//	cout << token << endl;
 			token = strtok(NULL, "&;|");
 		}
-		else
-		{
-			exit(0);
-		}	
+	
+		cout << "$ ";
+		getline(cin, user_input);
 	}
-
-
 
 
 	return 0;
