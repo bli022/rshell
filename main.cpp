11b4b717f5e208278fdd9b9ba24857c9572b9cf3
Breena Li <bli022@hammer.cs.ucr.edu>
2016-10-25 19:08:46 -0700

    added code to main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "rshell.h"
+
+using namespace std;
+
+int main() 
+{
+	string user_input;	
+	
+	cout << "Enter a command: " << endl;
+	cin >> user_input;
+
+
+	return 0;
+}
