0ab1fb57f694da5b914bc1c825377250ff7acc3f
Breena Li <bli022@hammer.cs.ucr.edu>
2016-10-29 22:17:35 -0700

    added a check for comments in user_input string

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cstdlib>
 #include <string.h>
 #include <stdio.h>
 
@@ -12,27 +13,42 @@ int main()
 {
 	string user_input;
 	string quit = "exit";
-
-	cout << "$ ";
-	getline(cin, user_input);
 	
-	while(user_input.compare(quit) != 0)
-	{	//FIX ME: parses fine. need to correctly put string into array
-		char *cmd_arr = new char[user_input.size() + 1];
-		const char d[4] = "&;|";
-		char *token = strtok(cmd_arr, d);
-		
-
-		while (token != NULL) 
-		{
-			printf(" %s\n", token);
-			token = strtok(NULL, d);
-		}
-	
+	do
+	{
 		cout << "$ ";
 		getline(cin, user_input);
-	}
 
+		if (user_input.compare(quit) != 0)
+		{
+			//check for a comment in the string
+			size_t pos = user_input.find('#');
+			user_input = user_input.substr(0, pos - 0);
+	
+			//convert user_input to a character array
+			char *cmd_arr = new char[user_input.length() + 1];
+			copy(user_input.begin(), user_input.end(), cmd_arr);
+			cmd_arr[user_input.size()] = '\0';
+			//delimiters for strtok function
+			const char d[4] = "&;|";
+			char *token = strtok(cmd_arr, d);
+		
+			//loop until end of token
+			while (token != NULL) 
+			{
+				printf(" %s\n", token);
+				token = strtok(NULL, d);
+			}
+		}
+
+		else
+		{
+			exit(0);
+		}
+
+	//loop until user wants to exit
+	} while(user_input.compare(quit) != 0);
+	
 
 	return 0;
 }
